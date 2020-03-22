12c00d93e8c6e377f5aa736fe0f4e60096d63c34
laripargea <61684338+laripargea@users.noreply.github.com>
2020-03-23 01:21:35 +0200

    final version

---
+++ Lab3/Lab3_test.cpp
@@ -0,0 +1,38 @@
+#include "Lab3_test.h"
+#include <cassert>
+
+//test for countNodes
+void test_countNodes()
+{
+	Tree tree(1);
+	tree.insert(2);
+	tree.insert(3);
+	assert(tree.countNodes() == 3);
+}
+
+//test for countEdges
+void test_countEdges()
+{
+	Tree tree(1);
+	tree.insert(2);
+	tree.insert(3);
+	assert(tree.countEdges() == 2);
+}
+
+//test the number of Leafs
+void test_countLeafs()
+{
+	Tree tree(1);
+	tree.insert(2);
+	tree.insert(3);
+	assert(tree.countNodes() == 3);
+}
+
+//test for height
+void test_height()
+{
+	Tree tree(1);
+	tree.insert(2);
+	tree.insert(3);
+	assert(tree.height() == 2);
+}
---
+++ Lab3/Lab3_test.h
@@ -0,0 +1,9 @@
+#pragma once
+#pragma once
+#include <iostream>
+#include "Tree.h"
+
+void test_countNodes();
+void test_countEdges();
+void test_countLeafs();
+void test_height();
