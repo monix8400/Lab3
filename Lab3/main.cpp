e2b2d17c362386b8a700da87cef5e3a5a7cf3980
MariaP78 <maria_minodora07@yahoo.com>
2020-03-23 01:13:22 +0200

    Am pus main-ul (part 2).

---
+++ Lab3/main.cpp
@@ -0,0 +1,22 @@
+#include "Tree.h"
+#include "Lab3_test.h"
+using std::cout;
+using std::endl;
+
+int main()
+{
+	Tree tree(40);
+	tree.insert(2);
+	tree.insert(70);
+	tree.insert(7);
+	tree.insert(700);
+	tree.insert(-1);
+	tree.insert(5);
+	tree.insert(200);
+	cout << "Inorder: " << endl;
+	cout << tree.inorder() << endl;
+	cout << "Postorder: " << endl;
+	cout << tree.postorder() << endl;
+	cout << "Preorder: " << endl;
+	cout << tree.preorder() << endl;
+}
